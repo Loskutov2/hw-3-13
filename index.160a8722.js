9d15a51859a92306d8aa5488e0befb00f5471919
Loskutov2 <112241103+Loskutov2@users.noreply.github.com>
2023-10-06 16:35:20 +0000

    Deploying to gh-pages from @ Loskutov2/hw-3-13@171228f0c525b42f3c97d712cc58a5f8a41ffc49 🚀

--- index.160a8722.js
+++
@@ -1,2 +0,0 @@
-!function(){var t="http://localhost:3000/movies";var n,o;n={title:"Harry Potter",director:"Somebody",genres:["Fantastic"],rating:10},o={method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(n)},fetch("".concat(t),o).then((function(t){return t.json}))}();
-//# sourceMappingURL=index.160a8722.js.map
--- index.9b4b7e60.js
+++
@@ -1,2 +0,0 @@
-!function(t){const o={method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(t)};fetch("http://localhost:3000/movies",o).then((t=>t.json))}({title:"Harry Potter",director:"Somebody",genres:["Fantastic"],rating:10});
-//# sourceMappingURL=index.9b4b7e60.js.map
